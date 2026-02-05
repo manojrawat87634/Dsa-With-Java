6fc469ba4b79913ee3fe46021a005076b0a6aec8
unknown <manojrawat87634@gmail.com>
2026-02-05 15:11:31 +0530

    inital commit

--- 01_core_java/04_operators/AirthmeticOperator/questions/Ans6.java
+++ 01_core_java/04_operators/AirthmeticOperator/questions/Ans6.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Ans6 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a two-digit number: ");
+        int num = sc.nextInt();
+
+        int firstDigit = num / 10;
+        int secondDigit = num % 10;
+
+        int sum = firstDigit + secondDigit;
+        System.out.println("Sum of digits = " + sum);
+        sc.close();
     }
 }
--- 01_core_java/04_operators/AirthmeticOperator/questions/Ans7.java
+++ 01_core_java/04_operators/AirthmeticOperator/questions/Ans7.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Ans7 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a two-digit number: ");
+        int num = sc.nextInt();
+
+        int firstDigit = num / 10;
+        int secondDigit = num % 10;
+
+        int product = firstDigit * secondDigit;
+        System.out.println("Product of digits = " + product);
+
+        sc.close();
     }
 }
